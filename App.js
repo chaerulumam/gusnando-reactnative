492139b55f14ff09e1c467e64be83794d551b981
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-03 21:09:19 +0700

    learn add button touchable opacity in rn

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, StatusBar, Image} from 'react-native';
+import {View, Text, StatusBar, Image, TouchableOpacity} from 'react-native';
 
 class App extends Component {
   constructor(props) {
@@ -24,7 +24,9 @@ class App extends Component {
           </Text>
         </View>
 
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+          onPress={() => console.log('halo')}
+          style={{justifyContent: 'center', alignItems: 'center'}}>
           <Image
             source={{
               uri: 'https://images.unsplash.com/photo-1662752531834-2d4606b7a27b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
@@ -32,7 +34,20 @@ class App extends Component {
             style={{width: 300, height: 300}}
             resizeMode="cover"
           />
-        </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#9c27b0',
+            paddingVertical: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 20,
+            marginHorizontal: 20,
+            borderRadius: 50,
+            elevation: 3,
+          }}>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>Click Here</Text>
+        </TouchableOpacity>
       </View>
     );
   }
